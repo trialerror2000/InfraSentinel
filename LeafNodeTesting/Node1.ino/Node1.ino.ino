bf62b89b143e83a523883f9a87040c633d744bff
trialerror2000 <RUDRASINGH961290@GMAIL.COM>
2026-04-29 20:31:46 +0530

    Final Code for the Leaf (sensing) nodes

--- LeafNodeTesting/Node1.ino/Node1.ino.ino
+++ LeafNodeTesting/Node1.ino/Node1.ino.ino
@@ -7,27 +7,16 @@
 #define NEO_PIN D4
 
 uint8_t receiverAddress[] = {0xC0, 0x5D, 0x89, 0xB1, 0x1C, 0x24};
-
-typedef struct struct_message {
-    int nodeID;
-    char sensorData[32];
-    int statusCode;
-} struct_message;
-
+typedef struct struct_message { int nodeID; float rawVal1; float rawVal2; } struct_message;
 struct_message myData;
 Adafruit_NeoPixel statusLED(1, NEO_PIN, NEO_GRB + NEO_KHZ800);
-bool deliverySuccess = false;
+bool successFlag = false;
 
-void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
-  deliverySuccess = (sendStatus == 0);
-}
+void OnDataSent(uint8_t *mac, uint8_t status) { successFlag = (status == 0); }
 
 void setup() {
-  Serial.begin(115200);
   statusLED.begin();
   WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
-
   if (esp_now_init() != 0) return;
   esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
   esp_now_register_send_cb(OnDataSent);
@@ -35,24 +24,20 @@ void setup() {
 }
 
 void loop() {
-  // Local read for serial monitor debugging
-  int rawVal = analogRead(SENSOR_PIN);
-  Serial.print("Local Moisture: "); Serial.println(rawVal);
-
   myData.nodeID = NODE_ID;
-  strcpy(myData.sensorData, "No Data"); // Sending literal text
-  myData.statusCode = 0;
+  myData.rawVal1 = analogRead(SENSOR_PIN); // Raw 0-1023
+  myData.rawVal2 = 0; // Unused for Node 1
 
   esp_now_send(receiverAddress, (uint8_t *) &myData, sizeof(myData));
 
-  if (!deliverySuccess) {
-    statusLED.setPixelColor(0, statusLED.Color(255, 0, 0)); // Blink RED
-    statusLED.show(); delay(200);
-    statusLED.setPixelColor(0, statusLED.Color(0, 0, 0));
-    statusLED.show(); delay(200);
-  } else {
-    statusLED.setPixelColor(0, statusLED.Color(0, 255, 0)); // Solid GREEN
+  if (successFlag) {
+    statusLED.setPixelColor(0, statusLED.Color(0, 255, 0)); // SOLID GREEN
     statusLED.show();
-    delay(1000);
+    delay(500);
+  } else {
+    statusLED.setPixelColor(0, statusLED.Color(255, 0, 0)); // RED BLINK
+    statusLED.show(); delay(150);
+    statusLED.setPixelColor(0, 0); 
+    statusLED.show(); delay(150);
   }
 }
